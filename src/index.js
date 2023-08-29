a8f9101f8453e8ebace6d2747b92f0a301973bc8
Saiakbai Nazarbekov <ff4rrell@gmail.com>
2023-08-29 13:40:38 +0600

    feat: add array sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const result = [];
+  if(matrix){
+    for(let i = 0; i < matrix.length; i++){
+      let arrayFromMatrix = matrix[i];
+      if((i % 2) === 0){
+        for(let k = 0; k < arrayFromMatrix.length; k++){
+          let firstNumbers = arrayFromMatrix[k];
+          result.push(firstNumbers);
+        }
+      }else{
+        for(let k = arrayFromMatrix.length - 1; k >= 0; k--){
+          let numbers = arrayFromMatrix[k];
+          result.push(numbers);
+        }
+      }
+    }
+  }
+  return result;
 }
